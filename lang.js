4aba127f4ea7e799fe5e13741afb10fd57485c81
Fraimy1052 <fraimymv35@gmail.com>
2025-07-21 13:02:49 -0400

    Update lang.js

--- lang.js
+++ lang.js
@@ -6,19 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
   function updateLanguage() {
     if (isSpanish) {
       btnCV.href = "docs/FraimyMendezVasquez_CV.pdf";
-      btnCV.textContent = "Descargar CV";
-      langToggle.textContent = "EN";
     } else {
       btnCV.href = "docs/FraimyMendezVasquez_Resume.pdf";
-      btnCV.textContent = "Download Resume";
-      langToggle.textContent = "ES";
     }
+    btnCV.textContent = isSpanish
+      ? btnCV.getAttribute("data-lang-es")
+      : btnCV.getAttribute("data-lang-en");
 
     document.querySelectorAll("[data-lang-es]").forEach((el) => {
       el.textContent = isSpanish
         ? el.getAttribute("data-lang-es")
         : el.getAttribute("data-lang-en");
     });
+
+    langToggle.textContent = isSpanish ? "EN" : "ES";
   }
 
   updateLanguage();
